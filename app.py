278a60685c5554f050f3fce07611eec1b08fca88
Ray Tan <tanjiav@gmail.com>
2024-09-29 11:00:17 -0400

    feq

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
-from flask import Flask, request, send_file, render_template
+from flask import Flask, request, send_file
 import os
+import threading
+import main  # Importing your main.py script
 
 app = Flask(__name__)
 
-# Set the uploads folder
 UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
@@ -23,16 +24,18 @@ def suggestions():
 def upload_file():
     if 'panoramaFile' not in request.files:
         return '<p class="text-red-600">No file part</p>'
-    
     file = request.files['panoramaFile']
-    
     if file.filename == '':
         return '<p class="text-red-600">No selected file</p>'
-    
     if file:
         filename = os.path.join(app.config['UPLOAD_FOLDER'], 'panoPhoto.png')
         file.save(filename)
-        return '<p class="text-green-600">File uploaded successfully!</p>'
+
+        # Run main.py in a separate thread
+        thread = threading.Thread(target=main.process_img)  # Assuming process_img is the function in main.py
+        thread.start()
+
+        return '<p class="text-green-600">File uploaded successfully! Image processing started.</p>'
 
 @app.route('/toronto.jpg')
 def serve_background():
